f6c8795b3204e31b48f6f0d22be337cbb5a5de4c
YassineKh2 <yassinkhemiri02@gmail.com>
2024-06-26 21:05:40 +0100

    First commit

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// Imports 
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require("body-parser");
 const http = require("http");
+
+// Routers Imports 
+const publicRouter = require("./routes/public.js");
 
 
 
@@ -15,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use("/public/images", express.static(__dirname + "/public/images/"));
+
+
+
+app.use("/", publicRouter);
 
 // Creating the server 
 const server = http.createServer(app);
